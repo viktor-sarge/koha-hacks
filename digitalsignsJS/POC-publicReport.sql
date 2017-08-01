d2489c24d988d320588f8bced056bf49463c6e56
Kultur i Halland - Regionbibliotek <viktor.sarge@regionhalland.se>
2017-08-01 08:12:29 +0200

    Added SQL for 17.05 since MARCXML has moved

--- digitalsignsJS/POC-publicReport.sql
+++ digitalsignsJS/POC-publicReport.sql
@@ -1,3 +1,7 @@
+/* 
+Code for Koha-versions prior to 17.05 (like 16.11)
+*/ 
+
 SELECT 
     biblio.biblionumber, 
     author, 
@@ -8,6 +12,27 @@ SELECT
 FROM biblio
 INNER JOIN biblioitems ON biblio.biblionumber = biblioitems.biblionumber
 LEFT JOIN items ON biblio.biblionumber = items.biblionumber
+WHERE isbn IS NOT NULL
+AND "desc" IS NOT NULL
+AND location = "BIBLIOTEK"
+GROUP BY biblio.biblionumber
+LIMIT 30
+
+/* 
+Code for 17.05 and later. (MARCXML has been moved in the database.)
+*/ 
+
+SELECT 
+    biblio.biblionumber, 
+    author, 
+    title, 
+    isbn,
+    location,
+    ExtractValue(biblio_metadata.metadata,'//datafield[@tag="520" AND @ind1="#" AND @ind2="#"]/subfield[@code="a"]') AS "desc"
+FROM biblio
+INNER JOIN biblioitems ON biblio.biblionumber = biblioitems.biblionumber
+LEFT JOIN items ON biblio.biblionumber = items.biblionumber
+INNER JOIN biblio_metadata ON biblio_metadata.biblionumber = biblio.biblionumber
 WHERE isbn IS NOT NULL
 AND "desc" IS NOT NULL
 AND location = "BIBLIOTEK"
