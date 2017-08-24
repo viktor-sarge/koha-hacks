054210dcf623d415f77b1f954c495407acd45e7a
Kultur i Halland - Regionbibliotek <viktor.sarge@regionhalland.se>
2017-08-24 13:45:16 +0200

    Added info on the technique used to the output

--- konstdatabas/SQL-report-for-opac-frontpage.sql
+++ konstdatabas/SQL-report-for-opac-frontpage.sql
@@ -1,4 +1,4 @@
-SELECT biblioimages.biblionumber, biblioimages.imagenumber, biblio.author, biblio.title, ExtractValue(biblioitems.marcxml,'//datafield[@tag="300"]/*') AS measurements
+SELECT biblioimages.biblionumber, biblioimages.imagenumber, biblio.author, biblio.title, ExtractValue(biblioitems.marcxml,'//datafield[@tag="300"]/*') AS measurements, v.lib
 FROM biblioimages
 INNER JOIN items
   ON items.biblionumber = biblioimages.biblionumber
@@ -8,6 +8,8 @@ LEFT JOIN biblio
   ON items.biblionumber = biblio.biblionumber
 LEFT JOIN biblioitems
   ON items.biblionumber = biblioitems.biblionumber
+LEFT JOIN authorised_values v
+  ON ExtractValue(biblioitems.marcxml,'//datafield[@tag="653"]/*') = v.authorised_value
 WHERE reserves.biblionumber IS NULL
 AND itemlost = 0
 AND onloan IS NULL
